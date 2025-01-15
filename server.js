9b085f8b467de3f8c3aa40c2d99ab7be061e95e0
xorazm92 <rayadunia2021@gmail.com>
2025-01-16 00:06:52 +0500

    EJS kamchiliklari

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 import app from './src/app.js';
 import { logger } from './src/utils/logger.js';
 import { config } from './src/configs/index.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Raya Dunia API',
+            version: '1.0.0',
+            description: 'API documentation for Raya Dunia',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./src/routers/*.js'],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const startServer = async () => {
     try {
